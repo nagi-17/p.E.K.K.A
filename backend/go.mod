8ab4b1d3e3abc9b847b37111a275c1ab3e0c4be2
nagi-17 <265831467+nagi-17@users.noreply.github.com>
2026-06-06 23:22:14 +0530

    write configs

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,5 @@
 module github.com/nagi-17/p.E.K.K.A
 
 go 1.26.4
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -1,1 +1,31 @@
 package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Port      string
+	DBHost    string
+	JWTSecret string
+}
+
+func LoadConfig() *Config {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
+
+	server_port := os.Getenv("SERVER_PORT")
+	db_host := os.Getenv("DB_HOST")
+	jwt_secret := os.Getenv("JWT_SECRET")
+
+	return &Config{
+		Port:      server_port,
+		DBHost:    db_host,
+		JWTSecret: jwt_secret,
+	}
+}
