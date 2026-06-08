ab75cffba5b87639805a04b65757ff1b6977cd67
nagi-17 <265831467+nagi-17@users.noreply.github.com>
2026-06-08 16:40:21 +0530

    fix: remove trophy_change attribute

--- backend/migration/000004_init_db_schema.up.sql
+++ backend/migration/000004_init_db_schema.up.sql
@@ -4,8 +4,6 @@ CREATE TABLE battle_log(
     defender_id UUID REFERENCES player_info(player_id) ON DELETE SET NULL,
     elixir_looted int NOT NULL,
     pancakes_looted int NOT NULL,
-    trophy_change_attacker int NOT NULL,
-    trophy_change_defender int NOT NULL,
     damage_percent float NOT NULL,
     time_of_battle TIMESTAMPTZ DEFAULT now()
 );
