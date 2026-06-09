5fd696d5016fc0b0455cd5fad8f95b0150784fed
nagi-17 <265831467+nagi-17@users.noreply.github.com>
2026-06-09 17:11:04 +0530

    chore: setup basic router

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/nagi-17/p.E.K.K.A/internal/config"
 	"github.com/nagi-17/p.E.K.K.A/internal/database"
+	"github.com/nagi-17/p.E.K.K.A/internal/routes"
 )
 
 func main() {
@@ -13,4 +15,10 @@ func main() {
 	database.Initialise_DB(config_data.DB_URL)
 
 	log.Printf("Server starting on port: %s\n", config_data.Port)
+	router := routes.InitRouter()
+
+	err := http.ListenAndServe(":"+config_data.Port, router)
+	if err != nil {
+		log.Fatalf("Server has crashed: %v\n", err)
+	}
 }
--- backend/internal/routes/routes.go
+++ backend/internal/routes/routes.go
@@ -1,1 +1,17 @@
 package routes
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/nagi-17/p.E.K.K.A/internal/controllers"
+)
+
+func InitRouter() *chi.Mux {
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
+
+	router.Post("/register", controllers.Register)
+	router.Post("/login", controllers.Login)
+
+	return router
+}
