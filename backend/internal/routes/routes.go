72fbc64d73c4427775ac35ee275a1f413746c9cd
nagi-17 <265831467+nagi-17@users.noreply.github.com>
2026-06-09 23:26:34 +0530

    chore: add jwt middleware in router

--- backend/internal/routes/routes.go
+++ backend/internal/routes/routes.go
@@ -2,16 +2,19 @@ package routes
 
 import (
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 	"github.com/nagi-17/p.E.K.K.A/internal/controllers"
+	"github.com/nagi-17/p.E.K.K.A/internal/middleware"
 )
 
 func InitRouter() *chi.Mux {
 	router := chi.NewRouter()
-	router.Use(middleware.Logger)
 
 	router.Post("/register", controllers.Register)
 	router.Post("/login", controllers.Login)
+
+	router.Group(func(protectedRoutes chi.Router) {
+		protectedRoutes.Use(middleware.VerifyJWT)
+	})
 
 	return router
 }
