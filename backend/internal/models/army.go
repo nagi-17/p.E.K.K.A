2a31dad5a3b95708120a38d88e1a6732db55e45f
nagi-17 <265831467+nagi-17@users.noreply.github.com>
2026-06-14 01:09:22 +0530

    chore: add function to train army

---
+++ backend/internal/models/army.go
@@ -0,0 +1,109 @@
+package models
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/google/uuid"
+	"github.com/nagi-17/p.E.K.K.A/internal/database"
+)
+
+func TrainArmy(ctx context.Context, playerID string, troopType string, quantity int) error {
+	playerUUID, err := uuid.Parse(playerID)
+	if err != nil {
+		return fmt.Errorf("Error in parsing player id: %w", err)
+	}
+
+	playerTroopInfo, err := GetPlayerTroopLevel(ctx, playerID, troopType)
+	if err != nil {
+		return fmt.Errorf("Error fetching player troop level: %w", err)
+	}
+
+	var trainTroopID, space int
+	query1 := `SELECT id, space_occupied_in_army FROM troop_data WHERE troop_type = $1 AND troop_level = $2`
+
+	err = database.DB.QueryRow(ctx, query1, troopType, playerTroopInfo.CurrentLevel).Scan(&trainTroopID, &space)
+	if err != nil {
+		return fmt.Errorf("Error fetching specified troop stats: %w", err)
+	}
+
+	query2 := `SELECT troop_data_id, quantity FROM trained_troop WHERE player_id = $1`
+	rows, err := database.DB.Query(ctx, query2, playerUUID)
+	if err != nil {
+		return fmt.Errorf("Error fetching trained army data: %w", err)
+	}
+	defer rows.Close()
+
+	var currSpace int
+	var allTrainedTroops []TrainedTroop
+	for rows.Next() {
+		var t TrainedTroop
+
+		err = rows.Scan(&t.TroopDataID, &t.Quantity)
+		if err != nil {
+			return fmt.Errorf("Error in scanning trained troop data/rows: %w", err)
+		}
+
+		allTrainedTroops = append(allTrainedTroops, t)
+		troopData, err := GetTroopStats(ctx, t.TroopDataID)
+		if err == nil {
+			currSpace += t.Quantity * troopData.SpaceOccupiedInArmy
+		}
+	}
+
+	var maxSpace int
+	allOwnedBuildings, err := GetOwnedBuildingData(ctx, playerUUID)
+
+	if err != nil {
+		return fmt.Errorf("Error in fetching army camps housing data: %w", err)
+	}
+
+	for i := 0; i < len(allOwnedBuildings); i++ {
+		if allOwnedBuildings[i].BuildingType == "Army Camp" {
+
+			if CheckUpgrading(ctx, allOwnedBuildings[i].ID) != nil {
+				continue
+			}
+
+			query3 := `SELECT housing_space FROM army_camp_data WHERE building_data_id = $1`
+			var tempSpace int
+
+			err = database.DB.QueryRow(ctx, query3, allOwnedBuildings[i].BuildingDataID).Scan(&tempSpace)
+			if err == nil {
+				maxSpace += tempSpace
+			}
+		}
+	}
+
+	if maxSpace == 0 {
+		return fmt.Errorf("All army camps are under upgrade")
+	}
+
+	spaceReq := quantity * space
+	if currSpace+spaceReq > maxSpace {
+		return fmt.Errorf("Insufficient housing space")
+	}
+
+	for i := 0; i < len(allTrainedTroops); i++ {
+		if allTrainedTroops[i].TroopDataID == trainTroopID {
+			newQuantity := quantity + allTrainedTroops[i].Quantity
+			query4 := `UPDATE trained_troop SET quantity = $1 WHERE player_id = $2 AND troop_data_id = $3`
+
+			_, err = database.DB.Exec(ctx, query4, newQuantity, playerUUID, trainTroopID)
+			if err != nil {
+				return fmt.Errorf("Failed to update troops in army: %w", err)
+			}
+			return nil
+		}
+	}
+
+	var id uuid.UUID = uuid.New()
+	query5 := `INSERT INTO trained_troop (id, player_id, troop_data_id, quantity) VALUES ($1, $2, $3, $4)`
+
+	_, err = database.DB.Exec(ctx, query5, id, playerUUID, trainTroopID, quantity)
+	if err != nil {
+		return fmt.Errorf("Failed to add troops to army: %w", err)
+	}
+
+	return nil
+}
