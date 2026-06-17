59fc40db9576f61559ec3a8222966c92120bd14b
nagi-17 <265831467+nagi-17@users.noreply.github.com>
2026-06-18 02:52:25 +0530

    feat: add battle/attack handler[backend]

--- backend/internal/controllers/battle.go
+++ backend/internal/controllers/battle.go
@@ -1,1 +1,64 @@
 package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/nagi-17/p.E.K.K.A/internal/models"
+	"github.com/nagi-17/p.E.K.K.A/internal/services"
+)
+
+type AttackRequest struct {
+	DefenderID string `json:"defender_id"`
+}
+
+func MatchMakeHandler(w http.ResponseWriter, request *http.Request) {
+	val := request.Context().Value("player_id")
+	playerID, ok := val.(string)
+	if !ok {
+		http.Error(w, "Player ID missing or is invalid in context", http.StatusInternalServerError)
+		return
+	}
+
+	defender, err := models.FindOpponent(request.Context(), playerID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusConflict)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(defender)
+}
+
+func AttackHandler(w http.ResponseWriter, request *http.Request) {
+	val := request.Context().Value("player_id")
+	playerID, ok := val.(string)
+	if !ok {
+		http.Error(w, "Player ID missing or is invalid in context", http.StatusInternalServerError)
+		return
+	}
+
+	var req AttackRequest
+	decoder := json.NewDecoder(request.Body)
+	err := decoder.Decode(&req)
+	if err != nil {
+		http.Error(w, "Bad request-wrong json payload", http.StatusBadRequest)
+		return
+	}
+
+	if req.DefenderID == "" {
+		http.Error(w, "Who will you attack? Defender ID empty", http.StatusBadRequest)
+		return
+	}
+
+	battleLog, err := services.Attack(request.Context(), playerID, req.DefenderID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusConflict)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(battleLog)
+}
