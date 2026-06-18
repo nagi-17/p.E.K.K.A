7237fe6bde251353703630b9295fe73867451b40
nagi-17 <265831467+nagi-17@users.noreply.github.com>
2026-06-18 15:23:19 +0530

    feat: add battle API endpoint[frontend]

---
+++ frontend/src/api/battle.js
@@ -0,0 +1,9 @@
+import { apiClient } from "./client";
+
+export async function findOpponent() {
+    return apiClient('/battle/matchmake', { method: 'GET' });
+}
+
+export async function launchAttack(defenderID) {
+    return apiClient('/battle/attack', { method: 'POST', body: JSON.stringify({defender_id: defenderID})});
+}
