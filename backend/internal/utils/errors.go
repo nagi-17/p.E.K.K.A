faab625e749aca79c16d1bf3286698b6c68148da
nagi-17 <265831467+nagi-17@users.noreply.github.com>
2026-07-09 17:36:04 +0530

    feat: add handle error function for controllers[backend]

---
+++ backend/internal/utils/errors.go
@@ -0,0 +1,30 @@
+package utils
+
+import (
+	"errors"
+	"log"
+	"net/http"
+)
+
+type UserError struct {
+	Message string
+}
+
+func (e *UserError) Error() string {
+	return e.Message
+}
+
+func HandleError(w http.ResponseWriter, err error, defaultMsg string, defaultStatus int) {
+	if err == nil {
+		return
+	}
+
+	log.Printf("Internal Error: %v", err)
+
+	var userErr *UserError
+	if errors.As(err, &userErr) {
+		http.Error(w, userErr.Message, http.StatusConflict)
+		return
+	}
+	http.Error(w, defaultMsg, defaultStatus)
+}
